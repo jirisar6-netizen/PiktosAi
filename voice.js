e62e122b5257dbc3b52849821fce2124045327f6
jirisar6-netizen <jirisar6@gmail.com>
2026-02-25 13:38:07 +0100

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,1 +1,10 @@
+export const voiceEngine = {
+    speak(text) {
+        const msg = new SpeechSynthesisUtterance(text);
+        msg.lang = 'cs-CZ';
+        msg.pitch = 1.2;
+        msg.rate = 0.9;
+        window.speechSynthesis.speak(msg);
+    }
+};
 
