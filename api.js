2c9c78d191390c0b15660fc5444c840452986627
jirisar6-netizen <jirisar6@gmail.com>
2026-02-25 13:40:50 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,14 @@
+const BASE_URL = 'https://api.arasaac.org/api/pictograms';
+
+export async function searchPictos(term) {
+    try {
+        // Hledáme piktogramy v češtině (cs)
+        const response = await fetch(`${BASE_URL}/cs/search/${encodeURIComponent(term)}`);
+        if (!response.ok) return [];
+        return await response.json();
+    } catch (error) {
+        console.error("Chyba při komunikaci s ARASAAC:", error);
+        return [];
+    }
+}
 
